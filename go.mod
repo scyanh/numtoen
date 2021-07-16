b36839c5c6651fdd180d7d1f20f1a39c4306d905
ALBERTO TORRES <scyanh@gmail.com>
2021-07-15 22:59:34 -0500

    add redis and docker compose

--- go.mod
+++ go.mod
@@ -6,6 +6,8 @@ require (
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751
 	github.com/gin-contrib/zap v0.0.1
 	github.com/gin-gonic/gin v1.6.3
+	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/onsi/gomega v1.14.0 // indirect
 	github.com/swaggo/files v0.0.0-20190704085106-630677cd5c14
 	github.com/swaggo/gin-swagger v1.3.0
 	github.com/swaggo/swag v1.6.9
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"github.com/go-redis/redis"
 	router "github.com/scyanh/numtoen/app/infrastructure/routes"
 	"go.uber.org/zap"
 	"os"
@@ -18,6 +20,21 @@ func main() {
 	httpRouter.INIT()
 	httpRouter.SETUP_MIDDLEWARES()
 	httpRouter.SETUP_ROUTES()
+
+	addr := os.Getenv("ENV_REDIS_LOCAL_URL")
+
+	c := redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	if err := c.Ping().Err(); err != nil {
+		zap.S().Errorw("Redis Initialize err", "addr", addr, "err", err)
+		return
+	}
+
+	fmt.Println("running redis ok")
 
 	//Run Server
 	port := os.Getenv("PORT")
